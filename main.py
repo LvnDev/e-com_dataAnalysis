f4419934f7ab7e323727cefbf1b3a668bcd65b34
LVN <lvn@something.com>
2025-01-09 14:26:49 +0000

    started working on processing the data. added basic data analysis.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from logging import exception
 
 import pandas as pd
@@ -5,7 +6,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+from PIL.ImageStat import Global
+from fontTools.varLib.models import nonNone
+
+#all global variables
 loaded_data = None #global variable for the loaded data
+total_transactions = None #global variable for the processed data of total transaction
+
+unique_store_locations = [] #store any found unique store locations in an array...
+unique_product_categories = [] #store any found unique product categories in an array...
+
+
 
 def start_interface():
     print(f"\nPlease select your option \n 1. Load Data \n 2. process data \n 3. visualise data \n 4. export data \n 5. exit program")
@@ -19,27 +30,49 @@ def start_interface():
         except ValueError: #catch if any input was not integer.
             print("Invalid input, enter a number with the following options: 1-5...")
 
-def load_data():
+def load_data(): #load data
     filename = input("\n CASE SENSITIVE do not worry about adding .csv in the end \n Enter a .CSV file name: ")
     global loaded_data
-    try:
+    try: #if no issues will continue as normal
         loaded_data = pd.read_csv(f"{filename}.csv")
         print(f"{loaded_data.head()}")
         input("Press ENTER to continue...")
         return
-    except FileNotFoundError:
+    except FileNotFoundError: #if not found then user returns to the options screen again
         print("File not found")
         input("Press ENTER to continue...")
         return
-    except Exception as e:
+    except Exception as e: #if some error happens the exception will pop up here and explain what the error may be.
         print(f"unexpected error: {e}")
         input("Press ENTER to continue...")
         return
 
 def process_data():
     print("\nProcessing data...")
+    global loaded_data
+    time.sleep(0.5) #small wait time
+    while True:
+        try: #small validation to prevent user stopping the program.
+            print(f"\n select an option... \n 1. retrieve details of specific store using TransactionID \n 2. continue to process data \n 3. exit processing of data")
+            usr_option = int(input("Enter your choice: "))
+            if 1 <= usr_option <=3:
+                break;
+            else:
+                print("Invalid choice, please pick the number between 1 and 3...")
+        except ValueError:
+            print("Invalid choice, please pick a number between 1 and 3...")
 
-
+    if usr_option == 1: #add a specific detail using transaction ID
+        print("add input code to retrieve details of specific store using TransactionID...")
+    elif usr_option == 2: #continue to process without needing to input transaction ID for a specific record.
+        global total_transactions
+        global unique_store_locations
+        total_transactions = loaded_data['TransactionID'].nunique() #number of unique. will count upto how many records
+        unique_store_locations = loaded_data['StoreLocation'].unique() #will store all store locations into an array.
+        print(unique_store_locations)
+        input("Press ENTER to continue...")
+    else: #this is quit processing data
+        return
 def visualise_data():
     print("")
     #conducting some small experiments of making bar chart.
@@ -63,6 +96,7 @@ while True: #added a while so that it wouldn't exit the program if user wants to
         export_data()
     if option == 5:
         print("Exiting program...")
+        time.sleep(1)
         exit()
 
 
